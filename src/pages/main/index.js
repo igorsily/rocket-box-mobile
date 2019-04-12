9083179786a2e30ae9ce29f03b32ac61f1baaa7c
IgorSily <igorsily2@gmail.com>
2019-04-12 01:38:30 -0300

    first commit

---
+++ src/pages/main/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+
+import styles from './styles';
+import logo from '../../assets/logo.png';
+import api from '../../services/api';
+import AsyncStorage from '@react-native-community/async-storage';
+
+export default class Main extends Component {
+  state = {
+    newBox: '',
+  };
+
+  async componentDidMount() {
+    const box = await AsyncStorage.getItem('@RocketBox:box');
+
+    if (box) {
+      this.props.navigation.navigate('Box');
+    }
+  }
+
+  handleSignIn = async () => {
+    const response = await api.post('boxes', {
+      title: this.state.newBox,
+    });
+
+    await AsyncStorage.setItem('@RocketBox:box', response.data._id);
+
+    this.props.navigation.navigate('Box');
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={logo} style={styles.logo} />
+        <TextInput
+          style={styles.input}
+          placeholder="Crie um box"
+          placeholderTextColor="#999"
+          autoCapitalize="none"
+          autoCorrect={false}
+          underlineColorAndroid="transparent"
+          onChangeText={text => this.setState({ newBox: text })}
+        />
+        <TouchableOpacity onPress={this.handleSignIn} style={styles.button}>
+          <Text style={styles.buttonText}>Criar</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
